66348b00640f65cdd597619e9496678ff2ceeef1
Nathan Rose <rosen4@my.erau.edu>
2019-06-05 15:47:28 -0400

    merge master with Branch One

---
+++ CodeOne.hpp
@@ -0,0 +1,31 @@
+/**	* CodeOne.hpp
+	* This is just a random file with a random class
+	* Just with a random class and other random stuff
+	* 
+	*/
+
+
+
+class randomClass {
+	//functions
+	public:
+	randomClass();
+	void foo();
+	
+	protected:
+	void bar();
+	
+	private:
+	void privateFoo();
+	
+	//variables
+	public:
+	
+	protected:
+	float thisThing;
+	float thatThing;
+	
+	private:
+	int someVariable;
+	double someOtherOne;
+}
